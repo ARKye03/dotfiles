3b98ee6d8aa331fce9eb2d8b6ec4dcf918bd10ec
ARKye03 <rafa2003sanchez@gmail.com>
2024-02-25 15:14:34 -0500

    ArchCraft Start

---
+++ themes/Windows/theme.bash
@@ -0,0 +1,109 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Onedark Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+
+##MyColors
+
+#Alacritty
+AlaBG='#1b1d25'
+
+#Polybar
+Polybar_Back_G='#363a4f'
+Polybar_Back_G_ALT='#2a2d41'
+Polybar_Play_Color='#62AEEF'
+Polybar_Foreground_Active_Workspace='#8e12d6'
+Polybar_Back_G2='#'
+Polybar_Back_G3='#'
+
+#Bspwmrc
+New_Pop_UP='#8aadf4'
+
+#Rofi
+Rofi_Background='#24273a'
+Rofi_AltBackground='#363a4f'
+Rofi_Foreground='#ABB2BF'
+Rofi_Selected='#8aadf4'
+Rofi_Active='#c6a0f6'
+Rofi_Urgent='#ed8796'
+
+
+#DefColors
+background='#363a4f'
+foreground='#ABB2BF'
+color0='#005f8b'
+color1='#E06B74'
+color2='#a6da95'
+color3='#E5C07A'
+color4='#ffffff'
+color5='#62AEEF'
+color6='#55B6C2'
+color7='#ffffff'
+color8='#ffffff'
+color9='#EA757E'
+color10='#A2CD83'
+color11='#EFCA84'
+color12='#6CB8F9'
+color13='#8aadf4'
+color14='#5FC0CC'
+color15='#B5BCC9'
+
+accent='#5294E2'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Arc-Easy'
+icon_theme='Colloid-purple-dracula-dark'
+cursor_theme='Qogirr'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x48'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$New_Pop_UP"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$New_Pop_UP"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
