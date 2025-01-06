f0ada4979a739fddd7af7168c5c0f335e86c919c
ARKye03 <rafa03-dev@proton.me>
2025-01-06 12:40:25 -0500

    Remove deprecated Lua notification scripts

--- scripts/lib.lua
+++
@@ -1,9 +0,0 @@
-local Lib = {}
-
-function Lib.Notify(message, urgency, timeout)
-    urgency = urgency or "normal"
-    timeout = timeout or 2000
-    os.execute('notify-send -u ' .. urgency .. ' -t ' .. timeout .. ' "' .. message .. '"')
-end
-
-return Lib
